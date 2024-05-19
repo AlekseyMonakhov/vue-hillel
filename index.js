44ef9220bbcea03ed7db7ec781e3a2e17b7b6142
Aleksey <goshagriboedskii@gmail.com>
2024-05-19 21:52:27 +0300

    updated

---
+++ index.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+sum(1, 2);
+
+subtract(2, 1);
+
+multiply(2, 3);
