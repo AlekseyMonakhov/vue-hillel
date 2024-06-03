8762f0422b558fd0d0669b620d6d0b3867ab704f
Aleksey <goshagriboedskii@gmail.com>
2024-06-03 23:52:48 +0300

    upd

---
+++ src/components/BtnGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="btn-group" role="group">
+        <button
+            type="button"
+            class="btn btn-secondary left"
+            :class="{ active: left }"
+            @click="toggleLeft"
+        >
+            Left
+        </button>
+        <button
+            type="button"
+            class="btn btn-secondary right"
+            :class="{ active: right }"
+            @click="toggleRight"
+        >
+            Right
+        </button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'BtnGroup',
+    data() {
+        return {
+            left: false,
+            right: false
+        }
+    },
+    methods: {
+        toggleLeft() {
+            this.left = !this.left
+            this.right = false
+        },
+        toggleRight() {
+            this.right = !this.right
+            this.left = false
+        }
+    }
+}
+</script>
